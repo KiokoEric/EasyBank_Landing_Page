1a4a81f48f56aeee82c29c223fbd06fb66ec0d69
kiokoeric <kiokoerick040@gmail.com>
2024-05-16 14:52:45 +0300

    feature(Hero): Creation of the Hero component in EasyBank Landing Page

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import HeroImage from '../../assets/image-mockups.png';
+import Button from '../Button/Button';
+
+const Hero = () => {
+return (
+    <div class='flex justify-between items-center pl-20'>
+        <section class=' flex flex-col gap-5 w-80'>
+            <h1 class='text-4xl'>Next generation digital banking</h1>
+            <p>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+            <Button />
+        </section>
+        <section className='HeroImage'>
+            <img src={HeroImage} alt='HeroImage' />
+        </section>
+    </div>
+)
+}
+
+export default Hero
